cfd5bc92db0bda32cc71acbcdd25666c813166bb
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-11 15:58:28 +0000

    Tested and implemented maximum temp for powersaving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,13 +9,27 @@ describe('Thermostat', function(){
     expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE);
   });
 
-  it('Should be able to increase', function() {
-    thermostat.increase(2);
-    expect(thermostat.temperature).toEqual(22);
+  describe('.increase', function(){
+    it('Should be able to increase', function() {
+      thermostat.increase(2);
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE + 2);
+    });
+
+    it('Should not exceed 25 when power saving mode active', function(){
+      thermostat.increase(6);
+      expect(thermostat.temperature).toEqual(thermostat.POWERSAVE_MAX);
+    });
   });
 
-  it('Should be able to decrease', function() {
-    thermostat.decrease(2);
-    expect(thermostat.temperature).toEqual(18);
+  describe('.decrease', function(){
+    it('Should be able to decrease', function() {
+      thermostat.decrease(2);
+      expect(thermostat.temperature).toEqual(thermostat.DEFAULT_TEMPERATURE - 2);
+    });
+
+    it('Should not decrease beyond 10', function(){
+      for (var i = 0; i < 11; i++) thermostat.decrease(1);
+      expect(thermostat.temperature).toEqual(thermostat.MINIMUM_TEMPERATURE);
+    });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,16 @@
 function Thermostat(){
   this.DEFAULT_TEMPERATURE = 20;
+  this.MINIMUM_TEMPERATURE = 10;
+  this.POWERSAVE_MAX = 25;
   this.temperature = this.DEFAULT_TEMPERATURE;
 };
 
 Thermostat.prototype.increase = function(n) {
   this.temperature += n;
+  if (this.temperature > this.POWERSAVE_MAX) this.temperature = this.POWERSAVE_MAX;
 };
 
 Thermostat.prototype.decrease = function(n) {
   this.temperature -= n;
+  if (this.temperature < this.MINIMUM_TEMPERATURE) this.temperature = this.MINIMUM_TEMPERATURE;
 };
