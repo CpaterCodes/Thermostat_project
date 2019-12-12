16ef0541cb9dff3734cea93c33964c8ff459b3cf
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-12 16:59:26 +0000

    Factored out page script into dedicated file, added options for more cities

---
+++ src/interfaceScript.js
@@ -0,0 +1,46 @@
+function weather(cityData,cityName){
+  $.get('http://api.openweathermap.org/data/2.5/weather?q='+ cityData +'&appid=41f6c1233674bb24b572c58a04f9c2eb&units=metric',function(data){
+    $('#city_temperature').text(data.main.temp);
+  });
+  $('#city').text(cityName);
+}
+
+function interface_init(){
+  var thermostat = new Thermostat();
+
+  $(document).ready(function(){
+    weather('London,uk','London');
+    $('#London').click(function(){
+      weather('London,uk','London');
+    });
+    $('#NewYork').click(function(){
+      weather('New York,us','New York');
+    });
+    $('#Ashgabat').click(function(){
+      weather('Ashgabat,tm','Ashgabat');
+    });
+    $('#temperature').text(thermostat.temperature);
+    $('#powersave').text('Ecological consideration mode');
+    $('#down').click(function(){
+      thermostat.decrease(1);
+      $('#temperature').text(thermostat.temperature);
+    });
+    $('#up').click(function(){
+      thermostat.increase(1);
+      $('#temperature').text(thermostat.temperature);
+    });
+    $('#powersave').click(function(){
+      if(thermostat.isPowerSave()){
+        thermostat.dangerMode();
+        $('#powersave').text('Ecological destruction mode');
+      } else {
+        thermostat.safetyMode();
+        $('#powersave').text('Ecological consideration mode');
+      };
+    });
+    $('#reset').click(function(){
+      thermostat.reset();
+      $('#temperature').text(thermostat.temperature);
+    });
+  });
+};
